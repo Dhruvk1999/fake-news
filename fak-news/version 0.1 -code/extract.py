b75128ee954e179ddfc463b1932519fcb76afef5
DhruvK1999 <dkanthaliya1999@gmail.com>
2020-12-16 15:54:17 +0530

    extracted price name and call

--- fak-news/version 0.1 -code/extract.py
+++ fak-news/version 0.1 -code/extract.py
@@ -3,29 +3,40 @@ import numpy as np
 import re
 
 
-def tip_filter(headline):
-    headline=headline.lower()
-    if 'buy' in headline:
-        return headline
-    elif 'sell' in headline:
-        return headline
-    if 'hold' in headline:
-        return headline
-    if 'accumulate' in headline:
-        return headline
-    if 'target' in headline:
-        return headline
-    
-    else: 
-        pass
-
-def price(headline):
-    price=re.compile(r'\d+\..\d+')
-    a=price.findall(headline)
+def price(tip):
+    res = [int(i) for i in tip.split() if i.isdigit()]
+    price=re.compile(r'\d+')
+    a=price.findall(tip)
     try:
         return float(a[0])
     except:
         pass
     else:
         return np.nan
+
+        
+    
+def company(tip):
+    bsh=re.compile(r'\s\w+\s\w+')
+    return bsh.findall(tip)[0]
+
+def advice(tip):
+    tip=tip.lower()
+    
+    if 'buy' in tip:
+        return 'buy'
+    elif 'sell' in tip:
+        return 'sell'
+    elif  'hold' in tip:
+        return 'hold'
+    elif  'accumulate' in tip:
+        return 'accumulate'
+
+    else: 
+        pass
+    
+    
+#for tip in tips:
+ #   print(price(tip),company(tip),advice(tip))
+
     
