cb28a1d2280fe5c55db2129d9c0affd95299d81c
DhruvK1999 <dkanthaliya1999@gmail.com>
2020-12-16 15:57:25 +0530

    crawler updated with filter

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
-'''for scarping page on moneycontrol.com stock section '''
+'''for scarping pages on moneycontrol.com stock section'''
 import bs4,requests
+import re
   
 headlines=[]   
         
@@ -20,3 +21,21 @@ for link in links:
 for hl in k:
     if len(hl)>25:
         headlines.append(hl)    
+        
+
+def tip_filter(headline):
+    headline=headline.lower()
+    if 'buy' in headline:
+        return headline
+    elif 'sell' in headline:
+        return headline
+    elif  'hold' in headline:
+        return headline
+    elif  'accumulate' in headline:
+        return headline
+    elif  'target' in headline:
+        return headline
+    else: 
+        pass
+    
+tips=list(filter(tip_filter,headlines))
